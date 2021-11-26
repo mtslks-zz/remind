927c74d5f751d881eefef0777e3702b331c1112b
Mathias Lukas <mathias.lukas@me.com>
2021-11-26 12:42:05 +0100

    Set postgres-env-vars for Heroku

--- util/database.js
+++ util/database.js
@@ -2,25 +2,6 @@ import camelcaseKeys from 'camelcase-keys';
 import dotenvSafe from 'dotenv-safe';
 import postgres from 'postgres';
 
-// import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
-
-// setPostgresDefaultsOnHeroku();
-
-module.exports = function setPostgresDefaultsOnHeroku() {
-  if (process.env.DATABASE_URL) {
-    const { parse } = require('pg-connection-string');
-
-    // Extract the connection information from the Heroku environment variable
-    const { host, database, user, password } = parse(process.env.DATABASE_URL);
-
-    // Set standard environment variables
-    process.env.PGHOST = host;
-    process.env.PGDATABASE = database;
-    process.env.PGUSERNAME = user;
-    process.env.PGPASSWORD = password;
-  }
-};
-
 dotenvSafe.config();
 
 function connectOneTimeToDatabase() {
